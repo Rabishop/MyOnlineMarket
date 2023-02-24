54d01b1fd5c4f24c4ae91f070de2d80a7ce2fa0d
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-02-24 19:02:35 +0900

    ADD User API

--- src/main.go
+++ src/main.go
@@ -1,8 +1,51 @@
 package main
 
-import "example.com/m/v2/module/user"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"example.com/m/v2/module/user"
+)
+
+// UserRegist API
+func UserRegistHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Check Method
+	if r.Method != "POST" {
+		jsonbyte, err := json.Marshal(user.UserRegistResponse{"Wrong Method"})
+		if err != nil {
+			fmt.Println("Marshal failed")
+		}
+		fmt.Fprintln(w, string(jsonbyte))
+	}
+
+	// Read Body
+	var userRegistRequest user.UserRegistRequest
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	json.Unmarshal([]byte(body), &userRegistRequest)
+
+	// Call Function
+	user.Regist(&userRegistRequest)
+
+	// jsonbyte, err := json.Marshal(UserRegistResponse{"Accepted"})
+	// if err != nil {
+	// 	fmt.Println("Marshal failed")
+	// }
+
+	// Return JSON
+	fmt.Fprintln(w, string(jsonbyte))
+}
 
 func main() {
 
-	user.Regist()
+	// ユーザ所持キャラクター一覧取得API
+	http.HandleFunc("/character/list", UserRegistHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
