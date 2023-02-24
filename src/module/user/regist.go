223d5f48f53891e17215025b1c99e3c24e8a72c8
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-02-24 19:02:56 +0900

    ADD Regist Function

--- src/module/user/regist.go
+++ src/module/user/regist.go
@@ -4,7 +4,19 @@ import (
 	"example.com/m/v2/model"
 )
 
-func Regist() error {
+// UserGetResponse struct
+type UserRegistResponse struct {
+	Status string `json:"status"`
+}
+
+// UserCreateRequest struct
+type UserRegistRequest struct {
+	UserAccount  string `json:"user_account"`
+	UserPassword string `json:"user_password"`
+	UserName     string `json:"user_name"`
+}
+
+func Regist(userRegistRequest *UserRegistRequest) error {
 
 	// connect database
 	DB := model.MysqlConn()
@@ -13,9 +25,9 @@ func Regist() error {
 	tx := DB.Begin()
 
 	user := new(model.User)
-	user.UserName = "Alice"
-	user.UserAccount = "Alice123"
-	user.UserPassword = "123456"
+	user.UserName = userRegistRequest.UserName
+	user.UserAccount = userRegistRequest.UserAccount
+	user.UserPassword = userRegistRequest.UserPassword
 
 	if err := tx.Debug().Create(&user).Error; err != nil {
 		tx.Rollback()
